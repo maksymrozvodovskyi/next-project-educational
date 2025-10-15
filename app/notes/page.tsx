01baf8b6e7e0445755ea6f0b8f34a5b7b5221c89
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-10-15 18:57:56 +0300

    add NoteItem, NoteList, and fetch data func

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,3 +1,13 @@
-export default function Notes() {
-  return <div>Notes</div>;
+import { getNotes } from "@/lib/api";
+import NoteList from "@/components/NoteList/NoteList";
+
+export default async function Notes() {
+  const response = await getNotes();
+
+  return (
+    <section>
+      <h1>Notes List</h1>
+      {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+    </section>
+  );
 }
