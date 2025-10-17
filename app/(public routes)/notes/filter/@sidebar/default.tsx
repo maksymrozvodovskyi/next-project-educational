7616f069d75d9c1b8a9647ef2bd303328cbf533d
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-10-17 16:27:39 +0300

    add sidebar

---
+++ app/(public routes)/notes/filter/@sidebar/default.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { getCategories } from "@/lib/api";
+
+export default async function NotesSidebar() {
+  const categories = await getCategories();
+
+  return (
+    <ul>
+      <li>
+        <Link href={`/notes/filter/all`}>All notes</Link>
+      </li>
+      {categories.map((category) => (
+        <li key={category.id}>
+          <Link href={`/notes/filter/${category.id}`}>{category.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
