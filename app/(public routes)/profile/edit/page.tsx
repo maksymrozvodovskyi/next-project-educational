ec6839ecf52f33193db051e2b653b5dc5e30da05
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-10-20 15:54:47 +0300

    save

--- app/(public routes)/profile/edit/page.tsx
+++ app/(public routes)/profile/edit/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit Profile",
+  description: "Edit your user details and settings",
+};
+
 export default function EditProfile() {
   return (
     <div>
